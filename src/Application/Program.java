19d0f9f76c564e598f26335aead830e339e09eb5
Osmar Reche Junior <osmar_reche@hotmail.com>
2021-02-26 01:39:02 -0300

    Projeto Meio

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -8,7 +8,14 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		int a, b, soma;
 		
+		System.out.println("Entre com o primeiro valor");
+		a = sc.nextInt();		
+		System.out.println("Entre com o segundo valor");
+		b = sc.nextInt();
+		soma = a + b;
+		System.out.println("A soma dos valores é: " + soma);
 		
 		
 		sc.close();
